07c0da333486f1e751d48364bbf2f581f6fa237d
Koroleva Irina <korolevai@gmail.com>
2022-08-13 00:31:30 +0300

    added functions for getiing random number

---
+++ 2-functions/index.js
@@ -0,0 +1,29 @@
+'use strict'
+
+const getPositiveInt = function(num) {
+    return Math.trunc(Math.abs(num));
+}
+// 1. Функция, возвращающая случайное целое положительное число из переданного диапазона включительно.
+const getRandomInt = function(num1, num2) {  
+    let max = Math.max(getPositiveInt(num1), getPositiveInt(num2));
+    let min = Math.min(getPositiveInt(num1), getPositiveInt(num2));
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let y = getRandomInt(-5, 11);
+console.log(y);
+// 2. Функция, возвращающая случайное целое четное число из переданного диапазона включительно.
+const getRandomOdd = function(num1, num2) {
+    let max = Math.ceil(Math.max(num1, num2));
+    let min = Math.floor(Math.min(num1, num2));
+    let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
+    if (randomInt % 2 === 0) {
+        randomInt +=1;
+    } if (randomInt > max) { 
+        randomInt -=2;
+    }
+    return randomInt;
+}
+
+let x = getRandomOdd(-10, 11);
+console.log(x);
