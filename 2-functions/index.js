16ce15da0e5b37bbd50b21d9dcd691c7b900c961
Koroleva Irina <korolevai@gmail.com>
2022-08-15 15:33:15 +0300

    converted functions to arrow functions

--- 2-functions/index.js
+++ 2-functions/index.js
@@ -1,10 +1,10 @@
 "use strict";
 
-const getPositiveInt = function (num) {
+const getPositiveInt = (num) => {
   return Math.trunc(Math.abs(num));
 };
 // 1. Функция, возвращающая случайное целое положительное число из переданного диапазона включительно.
-const getRandomInt = function (num1, num2) {
+const getRandomInt = (num1, num2) => {
   let max = Math.max(getPositiveInt(num1), getPositiveInt(num2));
   let min = Math.min(getPositiveInt(num1), getPositiveInt(num2));
   return Math.floor(Math.random() * (max - min + 1)) + min;
@@ -13,7 +13,7 @@ const getRandomInt = function (num1, num2) {
 // console.log(y);
 
 // 2. Функция, возвращающая случайное целое четное число из переданного диапазона включительно.
-const getRandomOdd = function (num1, num2) {
+const getRandomOdd = (num1, num2) => {
   let max = Math.ceil(Math.max(num1, num2));
   let min = Math.floor(Math.min(num1, num2));
   let randomInt = Math.floor(Math.random() * (max - min + 1) + min);
@@ -29,7 +29,7 @@ const getRandomOdd = function (num1, num2) {
 // console.log(x);
 
 // 3. Функция, возвращающая случайное положительное число с плавающей точкой из переданного диапазона включительно.
-const getRandomNumber = function (num1, num2, scope) {
+const getRandomNumber = (num1, num2, scope) => {
   let max = Math.max(Math.abs(num1), Math.abs(num2));
   let min = Math.min(Math.abs(num1), Math.abs(num2));
   return (Math.random() * (max - min) + min).toFixed(scope);
@@ -38,7 +38,4 @@ const getRandomNumber = function (num1, num2, scope) {
 // console.log(c);
 
 // 4. Функция для проверки максимальной длины строки.
-const isStringLength = function(str, length) {
-  return (str.length <= length) ? true : false;
-}
-// console.log(isStringLength ('Функция для проверки максимальной длины строки', 40));
+const isStringLength = (str, length) => (str.length <= length);
