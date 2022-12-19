7090e72c0047f202c68204eaf73cbc39131e6916
Koroleva Irina <korolevai@gmail.com>
2022-12-19 18:05:45 +0100

    converted div -> form

--- 5-todo-list/scripts.js
+++ 5-todo-list/scripts.js
@@ -3,8 +3,7 @@
 let list = document.querySelector('.todo-list');
 let input = document.querySelector('.todo-input');
 let priority = document.querySelector('.todo-priority');
-let add = document.querySelector('.todo-add');
-
+let form = document.querySelector('.todo-form');
 
 priority.onclick = () => {
   priority.classList.toggle('is-important');
@@ -12,17 +11,18 @@ priority.onclick = () => {
     priority.textContent = 'Важная задача';
   } else {
     priority.textContent = 'Обычная задача';
-   }
-}
+  }
+};
 
-add.onclick = () => {
+form.onsubmit = function (evt) {
+  evt.preventDefault();
   let newTask = document.createElement('li');
   if (input.value) {
-  if (priority.classList.contains('is-important')) {
-    newTask.classList.add('is-important');
+    if (priority.classList.contains('is-important')) {
+      newTask.classList.add('is-important');
+    }
+    newTask.textContent = input.value;
+    list.append(newTask);
+    input.value = '';
   }
-  newTask.textContent = input.value;
-  list.append(newTask);
-  input.value = '';
-}
-}
+};
